b7065dd6ad39eae165fe90b1366634abee0454e9
Amundsen <ChinaWhEnglish@outlook.com>
2024-08-26 17:45:08 +0800

    准备开始构建第一个小说网站引擎

--- downloader/engines/novel.py
+++ downloader/engines/novel.py
@@ -4,4 +4,10 @@
 # @Time: 03/04/2024 17:33
 # @Author: Amundsen Severus Rubeus Bjaaland
 
+from .tools import BookWeb
+
+
+class Engine1(BookWeb):
+    name = "久久小说网"
+    domains = ["www.338i.com"]
 
--- downloader/engines/tools.py
+++ downloader/engines/tools.py
@@ -7,51 +7,38 @@
 
 from abc import ABCMeta
 from typing import List
+
 from bs4 import Tag
-from urllib.parse import urlparse, ParseResult
 
-from ..books import Book, BookState
-
-
-class URL(object):
-    def __init__(self, url: str):
-        self.__url = url
-        self.__parse_result = urlparse(url)
-    
-    @property
-    def parse_result(self) -> ParseResult: return self.__parse_result
-    
-    @property
-    def url(self) -> str: return self.__url
-    
-    def __str__(self) -> str: return self.__url
+from ..books import Book
 
 
 class BookWeb(metaclass=ABCMeta):
-    domains: List[URL] = []
     name: str = ""
+    domains: List[str] = []
     book_url_pattern: str = ""
     chapter_url_pattern: str = ""
     section_url_pattern: str = ""
-    # TODO 测试功能
-    # author_url_pattern: str = ""
     encoding: str = "UTF-8"
     
-    def __str__(self) -> str: return self.name
+    def __str__(self) -> str:
+        return self.name
     
     def __repr__(self):
         return f"<WebConfig name={self.name} encoding={self.encoding}>"
     
     def get_book_info(self, book_html: Tag) -> Book:
         """获取书籍的基本信息"""
-        return Book("", "", BookState.END, "", "", b"")
+        return Book("", "", Book.State.END, "", "", b"")
     
-    def get_chapter_text(self, chapter_html: Tag, index: int) -> ChapterData:
+    def get_chapter_url(self, book_html: Tag) -> List[str]:
+        """获取书籍的章节来源"""
+        return []
+    
+    def get_chapter_text(self, chapter_html: Tag) -> List[str]:
         """获取章节的内容"""
-        response.response.encoding = self.encoding
-        return ChapterData(index + 1, "")
+        return []
     
     def is_protected(self, response: Network) -> bool:
         """判断是否为网站的访问保护"""
-        response.response.encoding = self.encoding
         return False
