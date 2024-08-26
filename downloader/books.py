2450c350e074ad66ca8c0919ce768a7de6371143
Amundsen <ChinaWhEnglish@outlook.com>
2024-08-26 17:22:50 +0800

    完成部分书籍保存功能

--- downloader/books.py
+++ downloader/books.py
@@ -7,18 +7,22 @@
 
 
 import os
+import copy
 import hashlib
 from typing import List, Tuple, Union
+
+import yaml
+from ebooklib import epub
 
 try:
     from .tools import mkdir
     from .settings import Settings
 except ImportError:
-    settings_flag = False
-    required_dirs = [
-        "./data", "./data/books",
-        "./data/books/cache", "./data/books/storage"
-    ]
+    class Settings:
+        DATA_DIR = os.path.abspath("./data")
+        BOOKS_DIR = os.path.abspath("./data/books")
+        BOOKS_CACHE_DIR = os.path.abspath("./data/books/cache")
+        BOOKS_STORAGE_DIR = os.path.abspath("./data/books/storage")
 
     def mkdir(path: str) -> None:
         """创建文件夹，若文件夹已存在则不进行任何操作
@@ -29,13 +33,11 @@ except ImportError:
             os.makedirs(path)
         except FileExistsError:
             pass
-else:
-    settings_flag = True
+finally:
     required_dirs = [
         Settings.DATA_DIR, Settings.BOOKS_DIR,
         Settings.BOOKS_CACHE_DIR, Settings.BOOKS_STORAGE_DIR
     ]
-finally:
     for i in required_dirs:
         mkdir(i)
 
@@ -69,15 +71,11 @@ class Chapter(object):
             case self.StorageMethod.MEMORY:
                 self.__content = list(text)
             case self.StorageMethod.DISK:
-                if settings_flag:
-                    self.__dir_path = \
-                        os.path.join(
-							Settings.BOOKS_CACHE_DIR,
-							book_name
-						)
-                else:
-                    self.__dir_path = \
-                        f"./data/books/cache/{book_name}"
+                self.__dir_path = \
+                    os.path.join(
+                        Settings.BOOKS_CACHE_DIR,
+                        book_name
+                    )
                 mkdir(self.__dir_path)
                 self.__file_path = os.path.join(self.__dir_path, f"{name}.txt")
                 with open(self.__file_path, "w", encoding="UTF-8") as txt_file:
@@ -90,7 +88,7 @@ class Chapter(object):
         return len(self.text)
 
     def __str__(self):
-        return f"第{str(self.__index).rjust(4, '0')}章" \
+        return f"第{self.str_index}章" \
             f" {self.__name}\n{self.text}\n\n"
 
     def __repr__(self):
@@ -98,7 +96,7 @@ class Chapter(object):
             f"name={self.__name} book_name={self.__book_name}>"
 
     def __hash__(self):
-        text = f"{str(self.__index).rjust(4, '0')}" \
+        text = f"{self.str_index}" \
             f"{self.name}{self.book_name}".encode()
         sha256_hash = hashlib.sha256(text)
         hash_value = sha256_hash.hexdigest()
@@ -117,6 +115,10 @@ class Chapter(object):
     @property
     def index(self):
         return self.__index
+    
+    @property
+    def str_index(self):
+        return str(self.__index).rjust(4, '0')
 
     @property
     def source(self):
@@ -155,7 +157,7 @@ class Chapter(object):
 
 
 class Book(object):
-    class BookState(object):
+    class State(object):
         """书籍的状态常量"""
         END = ("完结", 1)  # 已完结状态常量
         SERIALIZING = ("连载中", 2)  # 连载中状态常量
@@ -170,16 +172,36 @@ class Book(object):
                 if number == i[1]:
                     return i
             return cls.SERIALIZING
+    
+    class SaveMethod(object):
+        TXT = ("txt文件", 1, "txt")
+        EPUB = ("epub文件", 2, "epub")
+        PDF = ("pdf文件", 3, "pdf")
+
+        METHOD_LIST = (TXT, EPUB, PDF)
+
+        @classmethod
+        def transform(cls, number: int):
+            for i in cls.METHOD_LIST:
+                if number == i[1]:
+                    return i
+            return cls.EPUB
 
     def __init__(
             self, name: str, author: str, state: tuple,
-            source: str, desc: str = ""
+            source: str, desc: str = "", cover_image: bytes = b"",
+            **other_data
         ):
         self.__name: str = name
         self.__author: str = author
         self.__state: tuple = state
         self.__source: str = source
         self.__desc: str = desc
+        self.__cover_image: bytes = cover_image
+        try:
+            self.__other_data = other_data
+        except NameError:
+            self.__other_data = {}
         self.__chapter_list: List[Chapter] = []
 
     def __len__(self):
@@ -190,8 +212,7 @@ class Book(object):
             f"author={self.__author}>"
 
     def __hash__(self):
-        text = f"{self.name}{self.author}" \
-            f"{self.state[0]}".encode()
+        text = f"{self.name}{self.author}".encode()
         sha256_hash = hashlib.sha256(text)
         hash_value = sha256_hash.hexdigest()
         return int(hash_value, 16)
@@ -202,6 +223,34 @@ class Book(object):
             return True
         return False
 
+    @property
+    def index_list(self) -> List[int]:
+        index_list = [
+            i.index for i in self.__chapter_list
+        ]
+        index_list.sort()
+        return index_list
+    
+    @property
+    def chapter_list(self):
+        for i in self.__chapter_list:
+            yield i
+
+    def append(self, chapter: Chapter) -> bool:
+        if ((chapter.book_name != self.name) or
+            (chapter.index in self.index_list)):
+            return False
+        self.__chapter_list.append(chapter)
+        self.__chapter_list = \
+            sorted(
+                self.__chapter_list, key=lambda x: x.index
+            )
+        return True
+    
+    def save(self, method: tuple = SaveMethod.EPUB) -> int:
+        saver = Saver(self, method)
+        return saver.save()
+    
     @property
     def name(self):
         return self.__name
@@ -221,46 +270,94 @@ class Book(object):
     @property
     def desc(self):
         return self.__desc
-
-    def __index_list(self) -> List[int]:
-        index_list = [
-            i.index for i in self.__chapter_list
-        ]
-        index_list.sort()
-        return index_list
-
-    def append(self, chapter: Chapter) -> bool:
-        if ((chapter.book_name != self.name) or
-            (chapter.index in self.__index_list())):
-            return False
-        self.__chapter_list.append(chapter)
-        self.__chapter_list = \
-            sorted(
-                self.__chapter_list, key=lambda x: x.index
-            )
-        return True
+    
+    @property
+    def cover_image(self):
+        return self.__cover_image
+    
+    @property
+    def other_data(self):
+        return copy.deepcopy(self.__other_data)
 
 
 class Saver(object):
-    class SaveMethod(object):
-        ONE_TXT_FILE = ("一个txt文件", 1)
-        MANY_TXT_FILE = ("多个txt文件", 2)
-        EPUB = ("epub文件", 3)
-        PDF = ("pdf文件", 4)
-
-        METHOD_LIST = (ONE_TXT_FILE, MANY_TXT_FILE, EPUB, PDF)
-
-        @classmethod
-        def transform(cls, number: int):
-            for i in cls.METHOD_LIST:
-                if number == i[1]:
-                    return i
-            return cls.EPUB
-
-    def __init__(self, book: Book, method: tuple):
+    def __init__(
+        self, book: Book, method: tuple = Book.SaveMethod.EPUB
+    ):
         self.__book = book
         self.__method = method
-        self.__save()
+        self.__path = os.path.join(
+            Settings.BOOKS_STORAGE_DIR,
+            f"{book.name}.{method[2]}"
+        )
 
-    def __save(self):
-        pass
+    def save(self) -> int:
+        match self.__method:
+            case Book.SaveMethod.TXT:
+                return self.__txt_file()
+            case Book.SaveMethod.EPUB:
+                return self.__epub_file()
+            case Book.SaveMethod.PDF:
+                # TODO 完成该分支方法，使保存方式支持PDF文件
+                return 0
+            case _:
+                return 0
+    
+    def __txt_file(self):
+        with open(self.__path, "w", encoding="UTF-8") as txt_file:
+            txt_file.write(f"{self.__book.name} - {self.__book.author}\n\n")
+            txt_file.write(f"书籍简介:\n\t{self.__book.desc}\n\n")
+            for one_chapter in self.__book.chapter_list:
+                txt_file.write(str(one_chapter))
+        return os.path.getsize(self.__path)
+    
+    def __epub_file(self):
+        book = epub.EpubBook()
+        
+        book.set_title(self.__book.name)
+        book.set_language('zh-CN')
+        book.add_author(self.__book.author)
+        book.set_cover("image.jpg", self.__book.cover_image)
+        book.add_metadata('DC', 'description', self.__book.desc)
+        
+        if self.__book.other_data:
+            yaml_item = epub.EpubItem(
+                uid='yaml', file_name='metadata.yaml',
+                media_type='application/octet-stream',
+                content=yaml.dump(self.__book.other_data)
+            )
+            book.add_item(yaml_item)
+        
+        intro_e = epub.EpubHtml(
+            title='Introduction', file_name='intro.xhtml', lang='hr'
+        )
+        intro_e.content = (
+            f'<img src="image.jpg" alt="Cover Image"/>'
+            f'<h1>{self.__book.name}</h1>'
+            f'<p>{self.__book.desc}</p>'
+        )
+        book.add_item(intro_e)
+        
+        book.spine = ['nav', intro_e]
+        
+        for one_chapter in self.__book.chapter_list:
+            chapter_text_list = one_chapter.text.strip("\t").split("\n\t")
+            text = epub.EpubHtml(
+                title=one_chapter.name,
+                file_name=f'chapter_{one_chapter.str_index}.xhtml'
+            )
+            text.content = \
+                f'<h2 class="titlecss">{one_chapter.name}</h2>' + \
+                ''.join([f"<p>{i}</p>" for i in chapter_text_list])
+            book.spine.append(text)
+            book.add_item(text)
+        
+        book.add_item(epub.EpubNcx())
+        book.add_item(epub.EpubNav())
+        
+        epub.write_epub(self.__path, book, {})
+        
+        return os.path.getsize(self.__path)
+        
+        
+        
--- tests/test_books.py
+++ tests/test_books.py
@@ -12,20 +12,20 @@ from downloader.books import Chapter, Book
 
 class TestConstant:
     def test_book_state(self):
-        assert Book.BookState.transform(1) == Book.BookState.END
-        assert Book.BookState.transform(2) == Book.BookState.SERIALIZING
-        assert Book.BookState.transform(3) == Book.BookState.FORECAST
-        assert Book.BookState.transform(4) == Book.BookState.BREAK
-        assert Book.BookState.transform(5) == Book.BookState.SERIALIZING
-        assert Book.BookState.END == ("完结", 1)
-        assert Book.BookState.SERIALIZING == ("连载中", 2)
-        assert Book.BookState.FORECAST == ("预告", 3)
-        assert Book.BookState.BREAK == ("断更", 4)
-        assert Book.BookState.STATE_LIST == (
-            Book.BookState.END,
-            Book.BookState.SERIALIZING,
-            Book.BookState.FORECAST,
-            Book.BookState.BREAK
+        assert Book.State.transform(1) == Book.State.END
+        assert Book.State.transform(2) == Book.State.SERIALIZING
+        assert Book.State.transform(3) == Book.State.FORECAST
+        assert Book.State.transform(4) == Book.State.BREAK
+        assert Book.State.transform(5) == Book.State.SERIALIZING
+        assert Book.State.END == ("完结", 1)
+        assert Book.State.SERIALIZING == ("连载中", 2)
+        assert Book.State.FORECAST == ("预告", 3)
+        assert Book.State.BREAK == ("断更", 4)
+        assert Book.State.STATE_LIST == (
+            Book.State.END,
+            Book.State.SERIALIZING,
+            Book.State.FORECAST,
+            Book.State.BREAK
         )
 
     def test_storage_method(self):
@@ -41,10 +41,55 @@ class TestConstant:
             Chapter.StorageMethod.MEMORY,
             Chapter.StorageMethod.DISK
         )
+    
+    def test_save_method(self):
+        assert Book.SaveMethod.transform(1) == \
+            Book.SaveMethod.TXT
+        assert Book.SaveMethod.transform(2) == \
+            Book.SaveMethod.EPUB
+        assert Book.SaveMethod.transform(3) == \
+            Book.SaveMethod.PDF
+        assert Book.SaveMethod.TXT == ("txt文件", 1, "txt")
+        assert Book.SaveMethod.EPUB == ("epub文件", 2, "epub")
+        assert Book.SaveMethod.PDF == ("pdf文件", 3, "pdf")
+        assert Book.SaveMethod.METHOD_LIST == (
+            Book.SaveMethod.TXT,
+            Book.SaveMethod.EPUB,
+            Book.SaveMethod.PDF,
+        )
 
 
 class TestObject:
     def test_chapter(self):
+        test_chapter_1 = Chapter(
+            "TestChapter1", 1, "http://example.com/", "TestBook",
+            ("This is the first line.", "This is the second line.")
+        )
+        test_chapter_3 = Chapter(
+            "TestChapter1", 1, "http://example.com/", "TestBook",
+            ("This is the first line.", "This is the second line."),
+            Chapter.StorageMethod.DISK
+        )
+        
+        assert test_chapter_1.text == \
+            "\tThis is the first line.\n\tThis is the second line."
+        assert len(test_chapter_1) == 50
+        assert str(test_chapter_1) == "第0001章 TestChapter1\n\t" \
+            "This is the first line.\n\tThis is the second line.\n\n"
+        assert test_chapter_1 == test_chapter_3
+        
+        test_chapter_1.text = [
+            "This is the first line.", "This is the second line.",
+            "This is the third line."
+        ]
+        test_chapter_3.text = [
+            "This is the first line.", "This is the second line.",
+            "This is the third line."
+        ]
+        assert len(test_chapter_1) == 75
+        assert len(test_chapter_3) == 75
+    
+    def test_book(self):
         test_chapter_1 = Chapter(
             "TestChapter1", 1, "http://example.com/", "TestBook",
             ("This is the first line.", "This is the second line.")
@@ -54,14 +99,46 @@ class TestObject:
             ("This is the first line.", "This is the second line."),
             Chapter.StorageMethod.DISK
         )
-        test_chapter_3 = Chapter(
+        test_book_1 = Book(
+            "TestBook", "TestAuthor", Book.State.END,
+            "https://example.com/1/", "TestDescription_1"
+        )
+        test_book_2 = Book(
+            "TestBook", "TestAuthor", Book.State.SERIALIZING,
+            "https://example.com/2/", "TestDescription_2"
+        )
+        test_book_1.append(test_chapter_1)
+        test_book_1.append(test_chapter_2)
+        
+        assert len(test_book_1) == 2
+        assert len(test_book_2) == 0
+        assert test_book_1 == test_book_2
+        assert test_book_1.index_list == [1, 2]
+        assert list(test_book_1.chapter_list) == \
+            [test_chapter_1, test_chapter_2]
+    
+    def test_book_save(self):
+        with open("./tests/book.jpg", "rb") as pic_file:
+            pic = pic_file.read()
+        
+        test_book_1 = Book(
+            "TestBook", "TestAuthor", Book.State.END,
+            "https://example.com/1/", "TestDescription_1", pic
+        )
+        test_chapter_1 = Chapter(
             "TestChapter1", 1, "http://example.com/", "TestBook",
+            ("This is the first line.", "This is the second line.")
+        )
+        test_chapter_2 = Chapter(
+            "TestChapter2", 2, "http://example.com/", "TestBook",
             ("This is the first line.", "This is the second line."),
             Chapter.StorageMethod.DISK
         )
-        assert test_chapter_1.text == \
-            "\tThis is the first line.\n\tThis is the second line."
-        assert len(test_chapter_1) == 50
-        assert str(test_chapter_1) == "第0001章 TestChapter1\n\t" \
-            "This is the first line.\n\tThis is the second line.\n\n"
-        assert test_chapter_1 == test_chapter_3
+        test_book_1.append(test_chapter_1)
+        test_book_1.append(test_chapter_2)
+        
+        test_book_1.save(Book.SaveMethod.TXT)
+        test_book_1.save(Book.SaveMethod.EPUB)
+        
+        
+        
