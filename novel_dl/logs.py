3fb71bdce1e4d52296cb0edfc685999f70d1dcb6
amundsen <chinawhenglish@outlook.com>
2024-12-15 11:21:19 +0800

    初步更新日志模块

--- novel_dl/logs.py
+++ novel_dl/logs.py
@@ -8,51 +8,126 @@
 
 # 导入标准库
 import os
+import copy
 import logging
+import threading
+from typing import Dict, List
 
 # 导入自定义库
 from .settings import Settings
 
+# 导入第三方库
+import tqdm
 
-class Logger(object):
-	"""包装日志记录器，使其易于使用"""
-	def __init__(self, logger_name: str):
-		"""日志记录器初始化方法
-		
-		:param logger_name: 日志记录器的层级名称
-		"""
-		# 获取标准库中的记录器
-		self.__logger = logging.getLogger(logger_name)
-		# 设置日志记录器级别为DEBUG
-		self.__logger.setLevel(logging.DEBUG if Settings.DEBUG else logging.WARNING)
-		# 使子记录器的日志不向父记录器传递
-		self.__logger.propagate = False
-		
-		# 设置日志记录的格式
-		self.__formatter = logging.Formatter(Settings.LOG_FORMAT)
-		# 创建命令行日志显示器
-		self.__stream_handler = logging.StreamHandler()
-		# 创建日志文件写入器
-		self.__file_handler = logging.FileHandler(
-      		os.path.join(Settings.LOG_DIR, f"{Settings.LOG_FILE_NAME}.log"), encoding="UTF-8"
-    	)
-		# 设置命令行日志显示器的日志级别
-		self.__stream_handler.setLevel(logging.DEBUG if Settings.DEBUG else logging.INFO)
-		# 设置日志文件写入器的日志级别
-		self.__file_handler.setLevel(logging.WARNING)
-		# 设置日志处理器的日志格式
-		self.__stream_handler.setFormatter(self.__formatter)
-		self.__file_handler.setFormatter(self.__formatter)
-		
-		# 向日志记录器添加定义好的日志处理器
-		self.__logger.addHandler(self.__stream_handler)
-		self.__logger.addHandler(self.__file_handler)
-	
-	@property
-	def object(self):
-		"""获取日志记录器对象以便进行日志相关操作"""
-		return self.__logger
 
-	@property
-	def stream_handler(self):
-		return self.__stream_handler
+def synchronized(func):
+    func.__lock__ = threading.Lock()
+
+    def lock_func(*args, **kwargs):
+        with func.__lock__:
+            return func(*args, **kwargs)
+
+    return lock_func
+
+
+class Singleton(object):
+    __instance = None
+    
+    @synchronized
+    def __new__(cls):
+        if not cls.__instance:
+            cls.__instance = super(Singleton, cls).__new__(cls)
+        return cls.__instance
+
+
+class TqdmHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord):
+        try:
+            message = self.format(record)
+            tqdm.write(message)
+        except Exception:
+            self.handleError(record)
+
+
+class Logger(Singleton):
+    def __init__(self):
+        """整个包所使用的日志记录器"""
+        # 获取标准库中的记录器
+        self.__logger = logging.getLogger("novel_dl")
+        # 设置日志记录器级别为DEBUG
+        self.__logger.setLevel(logging.DEBUG if Settings.DEBUG else logging.WARNING)
+        # 使子记录器的日志不向父记录器传递
+        self.__logger.propagate = False
+        # 设置日志记录的格式
+        self.__formatter = logging.Formatter(Settings.LOG_FORMAT)
+        
+        # 创建处理器列表并初始化
+        self.__handlers: Dict[str, logging.Handler] = {}
+        self.__add_default_handlers()
+        # 创建配置方案列表并初始化
+        self.__scheme: Dict[str, List[str]] = {}
+        self.add_scheme("default", ["stream_handler", "file_handler"])
+        self.add_scheme("tqdm", ["tqdm_handler", "file_handler"])
+        # 设置默认的配置方案
+        self.__now_scheme: str = "default"
+        self.set_scheme(self.__now_scheme)
+    
+    def __call__(self):
+        return self.__logger
+    
+    def __add_default_handlers(self) -> Dict[str, logging.Handler]:
+        # 创建命令行日志显示器
+        stream_handler = logging.StreamHandler()
+        # 设置命令行日志显示器的日志级别
+        stream_handler.setLevel(logging.DEBUG if Settings.DEBUG else logging.INFO)
+        # 设置日志处理器的日志格式
+        stream_handler.setFormatter(self.__formatter)
+        # 添加命令行日志处理器到日志处理器列表
+        self.add_handler("stream_handler", stream_handler)
+        
+        # 创建日志文件写入器
+        file_handler = logging.FileHandler(
+              os.path.join(Settings.LOG_DIR, f"{Settings.LOG_FILE_NAME}.log"), encoding="UTF-8"
+        )
+        # 设置日志文件写入器的日志级别
+        file_handler.setLevel(logging.WARNING)
+        # 设置日志处理器的日志格式
+        file_handler.setFormatter(self.__formatter)
+        # 添加日志文件写入器到日志处理器列表
+        self.add_handler("file_handler", file_handler)
+        
+        # 创建进度条日志显示器
+        tqdm_handler = TqdmHandler()
+        # 设置进度条日志显示器的日志级别
+        tqdm_handler.setLevel(logging.DEBUG if Settings.DEBUG else logging.INFO)
+        # 设置进度条日志显示器的日志格式
+        tqdm_handler.setFormatter(self.__formatter)
+        # 添加进度条日志显示器到日志处理器列表
+        self.add_handler("tqdm_handler", tqdm_handler)
+    
+    def add_handler(self, name: str, handler: logging.Handler):
+        assert isinstance(name, str)
+        assert isinstance(handler, logging.Handler)
+        self.__handlers["name"] = copy.deepcopy(handler)
+    
+    def add_scheme(self, name: str, scheme: List[str]):
+        assert isinstance(name, str)
+        for i in scheme:
+            assert isinstance(i, str)
+            assert i in self.__handlers()
+        self.__scheme[name] = copy.deepcopy(scheme)
+    
+    def set_scheme(self, name: str):
+        if name not in self.__scheme:
+            return None
+        for i in self.__scheme[self.__now_scheme]:
+            self.__logger.removeHandler(self.__handlers[i])
+        for i in self.__scheme[name]:
+            self.__logger.addHandler(self.__handlers[i])
+        self.__now_scheme = name
+        return None
+    
+    @property
+    def object(self):
+        return self.__logger
+        
