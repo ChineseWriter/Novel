b1e26b0b12eae10ca9e4717afeb2fbb4a965c0e7
闻人星湘 <ChinaWhEnglish@outlook.com>
2025-03-09 20:21:44 +0800

    FUNC(utils): 将文件系统相关的操作独立出来

--- novel_dl/utils/options.py
+++ novel_dl/utils/options.py
@@ -7,17 +7,11 @@
 
 # 导入标准库
 import io
-import os
 import hashlib
 import threading
 
 # 导入 PIL 库
 from PIL import Image
-
-
-# Windows 路径中所有非法字符及其替代方案
-_ILLEGAL_CHARS = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
-_ILLEGAL_CHARS_REP = ['＜', '＞', '：', '＂', '／', '＼', '｜', '？', '＊']
 
 
 def singleton(cls):
@@ -51,35 +45,6 @@ def singleton(cls):
     
     # 返回装饰器函数
     return wrapper
-
-def mkdir(path: str) -> None:
-    """创建文件夹，若文件夹已存在则不进行任何操作
-    
-    :param path: 要创建的文件的路径
-    """
-    # 确保 path 是 str 类型
-    assert isinstance(path, str)
-    # 创建文件夹
-    try:
-        os.makedirs(path)
-    except FileExistsError:
-        pass
-
-
-def sanitize_filename(text: str) -> str:
-	"""替换非法字符
-	
-	:param text: 将要检查的字符串
-	:type text: str
-	
-	Example:
-		>>> sanitize_filename("测试字符串")
-	"""
-	# 检查每一个非法字符的存在情况, 如果存在则替换掉
-	for char, char_rep in zip(_ILLEGAL_CHARS, _ILLEGAL_CHARS_REP):
-		text = text.replace(char, char_rep)
-	# 返回替换以后的字符串
-	return text
 
 
 def hash(value: bytes | str, format: str = "BYTES") -> bytes | str:
