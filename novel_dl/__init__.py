a6679f09b97f82d9d2354e81d12ac24656779773
闻人星湘 <ChinaWhEnglish@outlook.com>
2025-03-18 12:54:18 +0800

    DOC(*): 准备第一版的发布事宜

--- novel_dl/__init__.py
+++ novel_dl/__init__.py
@@ -10,4 +10,12 @@ from . import core, utils, services
 
 
 # 更新说明
-VERSION = []
+VERSION = [
+    {
+        "Version": "0.0.1",
+        "Desc": [
+            "基本完成对项目的重构",
+            "下载一本小说的功能可以初步使用"
+        ]
+    },
+]
